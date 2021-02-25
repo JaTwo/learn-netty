2a7b1ce6dba9519063f23c28f67276f270b6ef60
JaTwo <jtwo_mwl@163.com>
2021-02-25 21:20:57 +0800

    完成基础entry
    Signed-off-by: JaTwo <jtwo_mwl@163.com>

--- first-stage/src/main/java/httpService/SimpleResponse.java
+++ first-stage/src/main/java/httpService/SimpleResponse.java
@@ -1,5 +1,7 @@
 package httpService;
 
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jtwo_
@@ -8,4 +10,58 @@ package httpService;
  * Description:
  */
 public class SimpleResponse {
+    private String method; //请求方法 GET或者POST
+    private String url; //请求url
+    private String version; //http版本
+    private Map<String, String> heads; //请求头
+    private String body;//请求体
+    private Map<String, String> params;//请求参数
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Map<String, String> getHeads() {
+        return heads;
+    }
+
+    public void setHeads(Map<String, String> heads) {
+        this.heads = heads;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
 }
